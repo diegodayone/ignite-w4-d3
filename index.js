47e9eb64a34073abd5cbe256dc88571d8a3193d3
diego <diego@getdayone.com>
2020-04-29 20:47:11 +0200

    final

--- index.js
+++ index.js
@@ -9,19 +9,43 @@ async function createEvent() {
         time: document.querySelector("#date").value,
         price: document.querySelector("#price").value,
     }
-    //POST the information to the endpoint
-    let response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(myEvent),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+
+    let response;
+
+    let id = document.querySelector("#current-event").value;
+    if (id){ //if we have an id, we update
+        //PUT to update the info
+        response = await fetch(url +"/" +id, { // https://striveschool.herokuapp.com/api/agenda/a2dsamkdj1dmsakd9012
+            method: "PUT",
+            body: JSON.stringify(myEvent),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        } )   
+    }
+    else{ //else we create!
+        //POST the information to the endpoint
+        response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(myEvent),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+   
     if (response.ok){
         let responseJson = await response.json()
-        document.querySelector("#current-events").innerHTML += `<li class="list-group-item">${myEvent.time}: ${myEvent.name} - ${myEvent.price}€</li>`
+        await loadEvents()
+        //document.querySelector("#current-events").innerHTML += `<li class="list-group-item">${myEvent.time}: ${myEvent.name} - ${myEvent.price}€</li>`
 
         console.log("Created!", responseJson)
+        document.querySelector("#name").value = ""
+        document.querySelector("#description").value = ""
+        document.querySelector("#date").value = ""
+        document.querySelector("#price").value = 0
+        document.querySelector("#create-button").value = "Create event"
+        document.querySelector("#current-event").value = ""
     }
     else{
         console.log("Error")
@@ -34,20 +58,23 @@ async function loadEvents() {
     if(response.ok){
         let events = await response.json()
         let currentEvents = document.querySelector("#current-events");
-        events.forEach(e => {
+        currentEvents.innerHTML = ""
+        events.forEach(singleEvent => {
             let li = document.createElement("li")
             li.className = "list-group-item"
-            li.innerText = `${e.time}: ${e.name} - ${e.price}€`
+            li.innerText = `${singleEvent.time.split("T")[0]}: ${singleEvent.name} - ${singleEvent.price}€`
+            let div = document.createElement("div")
             let editButton = document.createElement("button")
             editButton.className ="btn btn-primary"
             editButton.innerText="Edit"
-            editButton.addEventListener("click", () =>  editEvent(e))
-            li.appendChild(editButton)
+            editButton.addEventListener("click", () =>  editEvent(singleEvent))
+            div.appendChild(editButton)
             let deleteButton = document.createElement("button")
             deleteButton.className ="btn btn-danger"
             deleteButton.innerText = "X"
-            deleteButton.addEventListener("click", () => deleteEvent(e))
-            li.appendChild(deleteButton)
+            deleteButton.addEventListener("click", () => deleteEvent(singleEvent))
+            div.appendChild(deleteButton)
+            li.appendChild(div)
             currentEvents.appendChild(li)
         });
         // document.querySelector("#current-events").innerHTML = events.map(
@@ -72,8 +99,11 @@ async function editEvent(event){
     // take the information from the event and set them in the creation form
     document.querySelector("#name").value = event.name
     document.querySelector("#description").value = event.description
-    document.querySelector("#date").value = event.time
+    if (event.time)
+        document.querySelector("#date").value =  event.time.split("T")[0]
     document.querySelector("#price").value = event.price
+    document.querySelector("#current-event").value = event._id
+    document.querySelector("#create-button").value = "Update event"
     // modify the create method in order to EDIT the element instead of creating a new one!
 
 }
